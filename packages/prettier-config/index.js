b16ab1f34c544ee4c1f679460f7c3bcfc5cec003
Meagan Waller <meagan.waller@testdouble.com>
2023-09-17 19:24:49 -0400

    Upgrade prettier-config

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,8 +1,12 @@
 module.exports = {
   arrowParens: 'always',
+  bracketSpacing: true,
+  printWidth: 120,
   singleQuote: true,
   jsxSingleQuote: true,
   tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
   semi: false,
   plugins: ['prettier-plugin-prisma', 'prettier-plugin-tailwindcss'],
 }
